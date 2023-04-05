c96d0e5ce710e9af9beb49e0f22dd3d9dad07cff
Olya <109566838+Olyalya1999@users.noreply.github.com>
2023-04-05 13:29:18 -0400

    Add files via upload

---
+++ sprint_6/MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,43 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Olya on 04.04.2023.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovie, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_u8p3lgz5") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+        
+        func loadMovies(handler: @escaping (Result<mostPopularMovies, Error>) -> Void) {
+            
+            networkClient.fetch(url: mostPopularMoviesUrl) { result in
+                switch result {
+                case .success(let data):
+                    do {
+                        let mostPopularMovies = try JSONDecoder().decode(mostPopularMovies.self, from: data)
+                        handler(.success(mostPopularMovies))
+                    } catch {
+                        handler(.failure(error))
+                    }
+                case .failure(let error):
+                    handler(.failure(error))
+                }
+            }
+        }
+    }
+
