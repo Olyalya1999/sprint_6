827551055b8d5f0b3c69924b24ff747cac2a79ce
Olya <olyat1999@fmail.com>
2023-04-25 12:44:47 -0400

    Спринт 6 готов

--- MovieQuiz/Services/ QuestionFactory.swift
+++ MovieQuiz/Services/ QuestionFactory.swift
@@ -8,6 +8,21 @@
 import Foundation
 
 class QuestionFactory: QuestionFactoryProtocol {
+    private var movies: [MostPopularMovie] = []
+    private let moviesLoader: MoviesLoading
+    private weak var delegate: QuestionFactoryDelegate?
+    
+    
+    
+    init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?) {
+        self.moviesLoader = moviesLoader
+        self.delegate = delegate
+    }
+    
+    
+    
+    
+    /*
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
@@ -50,21 +65,54 @@ class QuestionFactory: QuestionFactoryProtocol {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
+    */
+
     
-    weak var delegate: QuestionFactoryDelegate?
+    func requestNextQuestion() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else {return}
+            let index = (0..<self.movies.count).randomElement() ?? 0
+            
+            guard let movie = self.movies [safe: index] else {return}
+            
+            var imageData = Data()
+            
+            do {
+                imageData = try
+                Data(contentsOf: movie.resizedImageURL)
+            } catch {
+                print("Failed to load image")
+            }
+            
+            let rating = Float(movie.rating) ?? 0
+            
+            let text = "Рейтинг этого фильма больше чем 7?"
+            let correctAnswer = rating > 7
+            
+            let question = QuizQuestion(image: imageData,
+                                        text: text,
+                                        correctAnswer: correctAnswer)
     
-    init(delegate: QuestionFactoryDelegate) {
-        self.delegate = delegate
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else
+                { return }
+                self.delegate?.didReceiveNextQuestion(question: question)
+            }
+        }
     }
     
-    
-    func requestNextQuestion()  {
-        guard let index = (0..<questions.count).randomElement() else
-        {  
-            return
+    func loadData() {
+        moviesLoader.loadMovies { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                switch result {
+                case .success(let mostPopularMovies):
+                    self.movies = mostPopularMovies.items
+                    self.delegate?.didLoadDataFromServer()
+                case .failure(let error):
+                    self.delegate?.didFailToLoadData(with: error)
+                }
+            }
         }
-        let question = questions [safe: index]
-        delegate?.didReceiveNextQuestion(question: question)// 3
     }
 }
-
